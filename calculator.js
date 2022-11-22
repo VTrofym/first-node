bea8b0f305851a4fd7e25cc2e5087910499ccaed
VTrofym <v.trof@icloud.com>
2022-11-22 20:49:32 +0200

    start

--- calculator.js
+++ calculator.js
@@ -5,6 +5,7 @@ console.log(process.argv);
 // const a = process.argv[2];
 // const b = process.argv[3];
 //аналогичная запись
+
 const [, , a, b] = process.argv;
 
 console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
